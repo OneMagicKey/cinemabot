7ca20e0952775d638de84a9292f238abf0d8773e
Daniil Tolstykh <daniil.tolstykh.1996@gmail.com>
2019-12-22 19:38:06 +0700

    test info

--- echo_bot_sample.py
+++ echo_bot_sample.py
@@ -17,7 +17,7 @@ def find_movie(message: types.Message):
     from imdb import IMDb
     ia = IMDb()
     user_id = message.from_user.id
-    movie = ia.search_movie(title=message.text)
+    movie = ia.search_movie(title=message.text, results=10).sort(key=lambda mov: mov['votes'], reverse=True)
     if movie:
         bot.send_message(user_id, movie[0].summary())
         bot.send_photo(user_id, movie[0]['full-size cover url'], movie[0]['title'])
