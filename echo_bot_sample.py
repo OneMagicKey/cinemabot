99775217701510670cdb0b192c8a52c1c644fc12
Daniil Tolstykh <daniil.tolstykh.1996@gmail.com>
2019-12-22 17:48:46 +0700

    test info

--- echo_bot_sample.py
+++ echo_bot_sample.py
@@ -1,5 +1,6 @@
 import os
 import config
+from telebot import types
 # from telebot import apihelper
 import telebot
 
@@ -7,14 +8,21 @@ import telebot
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello, I'm simple echo bot. Tell me something!")
+@bot.message_handler(commands=['start'])
+def start_command(message: types.Message):
+    user_id = message.from_user.id
+    bot.send_message(user_id, "Welcome to cinema bot")
 
 
-@bot.message_handler(commands=['lalala'])
-def send_mes(message):
-    bot.reply_to(message, "Irinana")
+def find_movie(message: types.Message):
+    from imdb import IMDb
+    ia = IMDb()
+    user_id = message.from_user.id
+    movie = ia.search_movie(title=message.text)
+    if movie:
+        bot.send_message(user_id, movie[0].get_current_info())
+    else:
+        bot.send_message(user_id, "Can't find" + message.text)
 
 
 @bot.message_handler(func=lambda m: True)
